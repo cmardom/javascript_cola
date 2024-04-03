07d8fdc83ce98440659e1c954cadb52262e063aa
Carmen <cmardom599@g.educaand.es>
2024-04-03 09:50:55 +0200

    cambiar estado: no funciona bien la cola

--- funciones.js
+++ funciones.js
@@ -20,10 +20,12 @@ function dibujarTarjetas(){
         etq_persona.className = "card col";
         let etq_img = document.createElement("img");
         etq_img.src = "images/joven3a.png";
-        etq_img.className = "card-img-top";
+        etq_img.className = "card-img-top w-50";
         etq_persona.append(etq_img);
         let etq_cardbody = document.createElement("div");
         etq_cardbody.className = "card-body";
+        etq_cardbody.id = persona.id;
+
         let etq_h5 = document.createElement("h5");
         etq_h5.innerText = persona.nombre;
         let etq_button = document.createElement("button");
@@ -44,21 +46,44 @@ function dibujarTarjetas(){
 
 function cambiarEstado(number){
     let etq_button = document.getElementById("boton");
-    
+    let etq_persona = document.getElementById(number);
+    let arrayElementos = etq_persona.children;
+    let botonPulsado = arrayElementos[1];
+
     if (turno === 0){
         let p = personas.find(x => x.id ===number);
+
         etq_turno.innerText = p.nombre;
         turno = p.id;
         etq_button.innerText = "Acabar turno";
         
-    } else {
+    } else if (turno !== number){
         if (!cola.includes(number)){
             cola.push(number);
             etq_cola.innerText = cola.join(",");
+            botonPulsado.innerText = "Esperando...";
+            
+            console.log(number);
+            console.log(arrayElementos);
+            
 
+        } 
+        
+    } else if (turno === number){
+        turno = 0;
+        // let nombrePrimeroCola = personas.find(x => x.id ===number)
+        for (const persona of personas) {
+            if (persona.id === cola[0]){
+                etq_turno.innerText = persona.nombre;
+                cola.shift();
+                let array_etq_cola = etq_cola.innerText.split(',');
+                array_etq_cola.shift();
+                etq_cola.innerText = array_etq_cola.toString();
+                
+
+            }
         }
-        
-        
+        botonPulsado.innerText = "Pedir turno";
     }
 
 }
